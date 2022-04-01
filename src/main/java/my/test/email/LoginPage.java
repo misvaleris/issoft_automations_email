97db8738c242881006c5bc08c4aa302fec214b63
misvaleris <vs.panteleeva@gmail.com>
2022-04-01 17:01:38 +0300

    task_70

--- src/main/java/my/test/email/LoginPage.java
+++ src/main/java/my/test/email/LoginPage.java
@@ -1,13 +1,14 @@
 package my.test.email;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class LoginPage {
@@ -30,12 +31,14 @@ public class LoginPage {
     WebDriver driver = CreateDriver.getInstance();
 
     public LoginPage() {
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
     public boolean login(String login, String password) {
 
         driver.navigate().to("https://mail.yandex.com/");
+        takeSnapShot(driver, "/Users/misvaleris/Documents/test.png");
+
         loginButton.click();
         String loginField = "//input[@id='passp-field-login']";
 
@@ -55,5 +58,17 @@ public class LoginPage {
         new WebDriverWait(driver, Duration.ofMinutes(1).plusSeconds(1)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".user-account_has-subname_yes > .user-account__name")));
         return userName.isDisplayed();
     }
+
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) {
+        try {
+            TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+            File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+            File DestFile = new File(fileWithPath);
+            FileUtils.copyFile(SrcFile, DestFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
