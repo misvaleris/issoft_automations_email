15adbbdd2effbe1fb890d014ea19eb7628cba5bc
misvaleris <vs.panteleeva@gmail.com>
2022-04-01 11:08:59 +0300

    task_60_POM

--- src/test/java/my/test/email/EmailMainTest.java
+++ src/test/java/my/test/email/EmailMainTest.java
@@ -1,23 +1,15 @@
 package my.test.email;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.openqa.selenium.WebDriver;
 
 public class EmailMainTest {
-    private LoginPage loginPage;
-    private LogoutPage logoutPage;
-    private WebDriver driver;
+    private final LoginPage loginPage = new LoginPage();
+    private final LogoutPage logoutPage = new LogoutPage();
 
     @BeforeEach
     void setup() {
-        loginPage = new LoginPage();
-        logoutPage = new LogoutPage();
-        driver = CreateDriver.getInstance();
     }
 
     @DisplayName("Login test for Email")
@@ -28,16 +20,13 @@ public class EmailMainTest {
     }
 
     @DisplayName("Logout test for Email")
-    @ParameterizedTest
-    @CsvFileSource(resources = "/EmailCredentials.csv", numLinesToSkip = 1)
-    void logoutTest(String login, String password) {
-        loginPage.login(login, password);
+    @Test
+    void logoutTest() {
         Assertions.assertTrue(logoutPage.logout(), "User don't log out");
     }
 
     @AfterEach
     void cleanup() {
-        driver.quit();
     }
 }
 
