12076d52de5844a0a089376f479186f100ec1b06
misvaleris <vs.panteleeva@gmail.com>
2022-04-09 15:15:08 +0300

    task_80

--- src/main/java/my/test/demo/table/DownloadData.java
+++ src/main/java/my/test/demo/table/DownloadData.java
@@ -1,17 +1,12 @@
 package my.test.demo.table;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 public class DownloadData {
     private final WebDriver driver;
@@ -20,24 +15,60 @@ public class DownloadData {
         this.driver = driver;
     }
 
+    private static final By SELECT_DOCX_BUTTON_XPATH = By.xpath("//a[contains(@href,'sample-doc-download')]");
+    private static final By SELECT_XLSX_BUTTON_XPATH = By.xpath("//a[contains(@href,'sample-xls-download')]");
+    private static final By SELECT_PDF_BUTTON_XPATH = By.xpath("//a[contains(@href,'sample-ppt-file')]");
+
+    private static final By DOWNLOAD_DOCX_BUTTON_XPATH = By.xpath("//a[contains(@href,'100kB.docx')]");
+    private static final By DOWNLOAD_XLSX_BUTTON_XPATH = By.xpath("//a[contains(@href,'XLSX_10.')]");
+    private static final By DOWNLOAD_PDF_BUTTON_XPATH = By.xpath("//a[contains(@href,'150kB')]");
+
+    private static final By DOWNLOADING_TITLE_XPATH = By.xpath("//em[text()='Please wait a moment']");
+
+
+
     public void downloadData() {
+
         String siteUrl = "https://file-examples.com/index.php/sample-documents-download/";
         driver.navigate().to(siteUrl);
 
-        WebElement selectDocxButton = driver.findElement(By.xpath("//a[contains(@href,'sample-doc-download')]"));
-        WebElement selectXlsxButton = driver.findElement(By.xpath("//a[contains(@href,'sample-xls-download')]"));
-        WebElement selectPdfButton = driver.findElement(By.xpath("//a[contains(@href,'sample-ppt-file')]"));
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(SELECT_DOCX_BUTTON_XPATH));
+        driver.findElement(SELECT_DOCX_BUTTON_XPATH).click();
 
-        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(selectDocxButton));
-        selectDocxButton.click();
-        WebElement downloadDocxButton = driver.findElement(By.xpath("//a[contains(@href,'100kB.docx')]"));
-        downloadDocxButton.click();
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOAD_DOCX_BUTTON_XPATH));
+        driver.findElement(DOWNLOAD_DOCX_BUTTON_XPATH).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOADING_TITLE_XPATH));
+        driver.findElement(DOWNLOADING_TITLE_XPATH).isDisplayed();
+
+        driver.navigate().to(siteUrl);
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(SELECT_XLSX_BUTTON_XPATH));
+        driver.findElement(SELECT_XLSX_BUTTON_XPATH).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOAD_XLSX_BUTTON_XPATH));
+        driver.findElement(DOWNLOAD_XLSX_BUTTON_XPATH).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOADING_TITLE_XPATH));
+        driver.findElement(DOWNLOADING_TITLE_XPATH).isDisplayed();
+
+        driver.navigate().to(siteUrl);
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(SELECT_PDF_BUTTON_XPATH));
+        driver.findElement(SELECT_PDF_BUTTON_XPATH).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOAD_PDF_BUTTON_XPATH));
+        driver.findElement(DOWNLOAD_PDF_BUTTON_XPATH).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(DOWNLOADING_TITLE_XPATH));
+        driver.findElement(DOWNLOADING_TITLE_XPATH).isDisplayed();
+
+
 
         //check if div with id="ad_position_box" exists
         //find close button
         //click it
-        WebElement downloadXlsxButton = driver.findElement(By.xpath("//a[contains(@href,'XLSX_10.')]"));
-        WebElement downloadPdfButton = driver.findElement(By.xpath("//a[contains(@href,'150kB')]"));
+
     }
 }
 
