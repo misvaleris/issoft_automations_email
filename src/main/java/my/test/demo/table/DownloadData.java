effed18df6b32dfc8ffe8ea0f1e6e16141e0fd5d
misvaleris <vs.panteleeva@gmail.com>
2022-04-09 14:45:28 +0300

    task_80

---
+++ src/main/java/my/test/demo/table/DownloadData.java
@@ -0,0 +1,41 @@
+package my.test.demo.table;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DownloadData {
+    private final WebDriver driver;
+
+    public DownloadData(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void downloadData() {
+        String siteUrl = "https://file-examples.com/index.php/sample-documents-download/";
+        driver.navigate().to(siteUrl);
+
+        WebElement selectDocxButton = driver.findElement(By.xpath("//a[contains(@href,'sample-doc-download')]"));
+        WebElement selectXlsxButton = driver.findElement(By.xpath("//a[contains(@href,'sample-xls-download')]"));
+        WebElement selectPdfButton = driver.findElement(By.xpath("//a[contains(@href,'sample-ppt-file')]"));
+
+        selectDocxButton.click();
+        WebElement downloadDocxButton = driver.findElement(By.xpath("//a[contains(@href,'100kB.docx')]"));
+        downloadDocxButton.click();
+
+        //check if div with id="ad_position_box" exists
+        //find close button
+        //click it
+        WebElement downloadXlsxButton = driver.findElement(By.xpath("//a[contains(@href,'XLSX_10.')]"));
+        WebElement downloadPdfButton = driver.findElement(By.xpath("//a[contains(@href,'150kB')]"));
+    }
+}
+
+
--- src/test/java/my/test/demo/DownloadDataTest.java
+++ src/test/java/my/test/demo/DownloadDataTest.java
@@ -1,26 +1,43 @@
 package my.test.demo;
 
-import org.junit.jupiter.api.*;
+import my.test.demo.table.DownloadData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DownloadDataTest {
-    private DownloadDataDemo downloadDataDemo;
+    private DownloadData downloadData;
     private WebDriver driver;
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
-        downloadDataDemo = new DownloadDataDemo(driver);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String parentDirectoryPath = System.getProperty("user.dir");
+        String downloadFilepath = parentDirectoryPath + "/target/";
+        File downloadFolder = new File(downloadFilepath);
+        if (!downloadFolder.exists()) {
+            downloadFolder.mkdir();
+        }
+        Map<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        driver = new ChromeDriver(options);
+        downloadData = new DownloadData(driver);
     }
 
     @DisplayName(value = "Loading the data Dynamically test")
     @Test
     void loadUserInfoTest() {
-        Assertions.assertEquals(downloadDataDemo.downloadData(), "0%","Data is not uploaded");
+        downloadData.downloadData();
     }
 
     @AfterEach
