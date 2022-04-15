ac78161ba924a76eb118ba9a1936d7f64eb1a43a
misvaleris <vs.panteleeva@gmail.com>
2022-04-15 17:55:01 +0300

    task_120

--- src/main/java/my/test/email/SauseTestWatcher.java
+++ src/main/java/my/test/email/SauseTestWatcher.java
@@ -2,12 +2,19 @@ package my.test.email;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.rules.TestWatcher;
+import org.openqa.selenium.WebDriver;
 
 public class SauceTestWatcher extends TestWatcher {
+
+    private final WebDriver driver;
+
+    public SauceTestWatcher() {
+        this.driver = CreateDriver.getInstance().getDriver();
+    }
+
     @Override
-    public void testSuccessful(ExtensionContext context) {
+    public void testSuccessful(ExtensionContext context, Throwable throwable) {
         driver.executeScript("sauce:job-result=passed");
-        driver.quit();
     }
 
     @Override
