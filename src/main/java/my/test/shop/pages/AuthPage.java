aa8fdaf8b142c2c7bc99172396e1e2ba1827d62c
misvaleris <vs.panteleeva@gmail.com>
2022-04-22 11:06:08 +0300

    final task

--- src/main/java/my/test/shop/pages/AuthPage.java
+++ src/main/java/my/test/shop/pages/AuthPage.java
@@ -1,4 +1,23 @@
 package my.test.shop.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AuthPage {
+
+    @FindBy(xpath = "//input[@id='email_create']")
+    private WebElement emailRegistrationFiled;
+
+    @FindBy(xpath = "//button[@name='SubmitCreate']")
+    private WebElement createAccountButton;
+
+    @FindBy(xpath = "//input[@id='email']")
+    private WebElement emailField;
+
+    @FindBy(xpath = "//input[@id='passwd']")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//button[@name='SubmitLogin']")
+    private WebElement signInButton;
+
 }
