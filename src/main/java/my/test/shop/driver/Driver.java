fb89f6101c5672ae212da29e7f31bc7db70b94ec
misvaleris <vs.panteleeva@gmail.com>
2022-04-22 11:03:19 +0300

    final task

--- src/main/java/my/test/shop/driver/Driver.java
+++ src/main/java/my/test/shop/driver/Driver.java
@@ -1,4 +1,35 @@
 package my.test.shop.driver;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class Driver {
+
+    private WebDriver driver;
+    private static Driver instance;
+
+    private Driver() {
+        driver = new ChromeDriver();
+    }
+
+    public synchronized static Driver getInstance() {
+        if (instance == null) {
+            instance = new Driver();
+        }
+        return instance;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public static String getInfo() {
+        Capabilities cap = ((RemoteWebDriver) getInstance().getDriver()).getCapabilities();
+        String browserName = cap.getBrowserName();
+        String platform = cap.getPlatformName().toString();
+        String version = cap.getBrowserVersion();
+        return String.format("browser: %s v: %s platform: %s", browserName, version, platform);
+    }
 }
